c7d9085e722ab186663069c86b2ed02cbd8502a5
Eleonora Mattera <m.eleonora98@live.it>
2023-09-22 13:19:50 +0200

    security

---
+++ RegisterLoginLogout/src/main/java/com/example/RegisterLoginLogout/RegisterLoginLogoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.RegisterLoginLogout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLoginLogoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterLoginLogoutApplication.class, args);
+	}
+
+}
---
+++ RegisterLoginLogout/src/test/java/com/example/RegisterLoginLogout/RegisterLoginLogoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RegisterLoginLogout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginLogoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
